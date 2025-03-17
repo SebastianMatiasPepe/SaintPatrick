ec0a0bafa4156ac6c02d18380134a89d748a19c9
Sonny Pimentel <spimentelm1201@gmail.com>
2025-03-17 14:05:10 -0500

    Update main.jsx
    Fix app

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom";
 import './css/index.css'
-import App from './App';
+import App from './app';
 
 
 
